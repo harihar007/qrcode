4f41c86d9c42690e2c055708ab8e792782bd2370
harihar007 <67547306+harihar007@users.noreply.github.com>
2020-07-20 15:27:20 +0530

    Add files via upload

---
+++ sanjay.py
@@ -0,0 +1,10 @@
+import qrcode
+
+code=qrcode.QRCode(version=5,box_size=15,border= 5 )
+  
+link=input("Copy the content:")
+code.add_data(link)
+code.make(fit=True)
+img=code.make_image(fill="black", back_color="white")
+img.save(input("Enter the Qrcode name:")+".png")
+
